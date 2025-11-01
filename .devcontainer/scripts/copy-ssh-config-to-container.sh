9dea2fbe97996b176e388cc75271df5d8bfbb3a1
Gordan Nekić <gordan@neki.ch>
2025-11-01 22:42:38 +0000

    feat: :art: improve the default structure add todo

--- .devcontainer/scripts/copy-ssh-config-to-container.sh
+++ .devcontainer/scripts/copy-ssh-config-to-container.sh
@@ -2,3 +2,6 @@
 
 # Copy ssh config file to the container
 cp .devcontainer/.secrets/SSH/config ~/.ssh/config
+
+# Resolve permission errors for keys ("Permissions 0644 for 'key' are too open.")
+chmod 600 .devcontainer/.secrets/KEYS/*
